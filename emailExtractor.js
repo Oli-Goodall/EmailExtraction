19445aa28a6461c1bfab9a93e30a4747d583d8f0
Oli Goodall <Oli.Goodall@softwire.com>
2022-08-11 11:58:33 +0100

    Working dictionary

--- emailExtractor.js
+++ emailExtractor.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const { getMaxListeners } = require('process');
   
 
 const data = fs.readFileSync('./test.txt',
@@ -17,3 +18,44 @@ for(i = 0; i < wordArray.length; i++) {
 console.log(counter)
 
 //RegEx for this: [^ \r\n]*@softwire\.com\b
+
+let emailAddresses = data.match(/[^ \r\n]*@*\.*\b/g)
+
+let dict = {};
+
+let domainNames = []
+
+for(i = 0; i < emailAddresses.length; i++){
+  if (emailAddresses[i].match(/@.*\..*\b/g)){
+    domainNames.push(emailAddresses[i].match(/@.*\..*\b/g))
+  }
+}
+
+// dict["key"] = 1
+
+// for(i = 0; i < domainNames.length; i++){
+//   if (!dict[domainNames[i]]){
+//     dict[domainNames[i]] = 1
+//   }
+// }
+
+domainNames.forEach(domainName => {
+  if (!dict[domainName]){
+        dict[domainName] = 1
+      } 
+})
+
+
+
+// console.log(dict["key"])
+
+
+console.log(dict)
+//console.log(emailAddresses.length)
+
+
+// dict = {
+//   @softwire.com : 1,
+//   @techswitch.co.uk : 1,
+//   @getmail.com : 1
+// }
